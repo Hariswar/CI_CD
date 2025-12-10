c734e45d2fec741a88b1e254f4fa0180477df33b
Hey, I'm Hariswar <70029747+Hariswar@users.noreply.github.com>
2025-12-10 18:49:51 +0000

    additions

--- tests /test_app.py
+++ tests /test_app.py
@@ -5,9 +5,17 @@ root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
 from app import add 
+from app import subtract 
+from app import multiply
 
 def test_add():
     assert add(5, 6) == 11
 
 def test_add2():
     assert add(5, 6) != 10
+
+def test_subtract():
+    assert subtract(20, 15) == 5
+
+def test_multiply():
+    assert multiply(20, 15) == 300
